f375aad3a352c4b6f9cb7732537254015140d299
ztaaso <thiago@bhsoftwares.com.br>
2021-08-18 23:36:37 -0300

    finishing cart container

--- src/screens/Private/Sales/screens/SalesPoint/screens/Sell/components/CartContainer/index.tsx
+++ src/screens/Private/Sales/screens/SalesPoint/screens/Sell/components/CartContainer/index.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
 import CustomText from '~/src/components/CustomText';
 
-import { Container } from './styles';
+import { Feather, FontAwesome5 } from '@expo/vector-icons';
+
+import {
+  Container,
+  EntityContainer,
+  EntityBlock,
+  EntityIconContainer,
+  CartContainer,
+  CartInfo,
+  ChargeButtonContainer,
+  ChargeButton,
+  CartIconContainer,
+  CartTextInfo,
+} from './styles';
 
 const SalesPointCartContainer: React.FC = () => (
   <Container>
-    <CustomText>Em breve...</CustomText>
+    <EntityContainer>
+      <EntityBlock>
+        <EntityIconContainer>
+          <FontAwesome5 name="user-tag" size={24} color="#30d88b" />
+        </EntityIconContainer>
+        <CustomText font="Montserrat_Bold" fontSize="16">
+          Vendedor
+        </CustomText>
+      </EntityBlock>
+      <EntityBlock hasBorderLeft>
+        <EntityIconContainer>
+          <FontAwesome5 name="user-alt" size={24} color="#30d88b" />
+        </EntityIconContainer>
+        <CustomText font="Montserrat_Bold" fontSize="16">
+          Comprador
+        </CustomText>
+      </EntityBlock>
+    </EntityContainer>
+    <CartContainer>
+      <CartInfo>
+        <CartIconContainer>
+          <Feather name="shopping-cart" size={28} color="#30D88B" />
+        </CartIconContainer>
+        <CartTextInfo>
+          <CustomText
+            fontSize="16"
+            font="Montserrat_Bold"
+            numberOfLines={1}
+            style={{ maxWidth: 130 }}
+          >
+            280 Produtos
+          </CustomText>
+          <CustomText fontSize="14" numberOfLines={1} style={{ maxWidth: 130 }}>
+            R$ 20.000,00
+          </CustomText>
+        </CartTextInfo>
+      </CartInfo>
+      <ChargeButtonContainer>
+        <ChargeButton>
+          <CustomText color="#000" font="Montserrat_Bold">
+            Cobrar
+          </CustomText>
+        </ChargeButton>
+      </ChargeButtonContainer>
+    </CartContainer>
   </Container>
 );
 
--- src/screens/Private/Sales/screens/SalesPoint/screens/Sell/components/CartContainer/styles.ts
+++ src/screens/Private/Sales/screens/SalesPoint/screens/Sell/components/CartContainer/styles.ts
@@ -1,7 +1,84 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import { css } from 'styled-components';
 
 export const Container = styled.View`
-  border: 1px solid green;
-  background-color: #1a1f24;
+  /* flex: 1; */
+
+  background-color: #000;
+  /* padding: 20px; */
+
+  border-top-color: rgba(255, 255, 255, 0.2);
+  /* border-top-width: 1px; */
+  /* border-top-right-radius: 20px; */
+  /* border-top-left-radius: 20px; */
+`;
+
+export const EntityContainer = styled.View`
+  /* flex: 1; */
+
+  flex-direction: row;
+`;
+
+export const EntityBlock = styled.TouchableOpacity`
+  flex: 1;
+  flex-direction: row;
   padding: 20px;
+
+  ${(props: { hasBorderLeft: boolean | null }) =>
+    props.hasBorderLeft &&
+    css`
+      border-left-color: rgba(255, 255, 255, 0.2);
+      border-left-width: 1px;
+
+      /* border-bottom-left-radius: 10px; */
+      /* border-top-left-radius: 10px; */
+    `}
+`;
+
+export const EntityIconContainer = styled.View`
+  padding: 0 10px;
+`;
+
+export const CartContainer = styled.View`
+  /* flex: 1; */
+  flex-direction: row;
+  padding: 15px 0;
+`;
+
+export const CartInfo = styled.View`
+  flex-direction: row;
+  flex: 0.9;
+
+  /* border: 1px solid blue; */
+
+  /* padding: 0 10px; */
+`;
+
+export const ChargeButtonContainer = styled.View``;
+
+export const ChargeButton = styled(RectButton)`
+  flex: 1;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: #30d88b;
+  padding: 0px 50px;
+  border-radius: 10px;
+`;
+
+export const CartIconContainer = styled.TouchableOpacity`
+  padding: 0 8px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CartTextInfo = styled.View`
+  /* flex: 1; */
+  padding: 0 10px;
+  /* border: 1px solid blue; */
+
+  /* flex: 1; */
 `;
